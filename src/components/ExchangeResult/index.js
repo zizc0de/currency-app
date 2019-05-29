d330259350de14c39bb713b96c86d233fa273f2f
zizc0de <zizcode.exporadev@gmail.com>
2019-05-29 15:17:10 +0700

    Added ExchangeResult component

--- src/components/ExchangeResult/index.js
+++ src/components/ExchangeResult/index.js
@@ -1,67 +1,70 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import { find } from 'lodash';
 
 import './_styles.scss';
 
-class ExchangeResult extends React.Component {
+class ExchangeResult extends Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  removeCurrency = (e, key) => {
+    e.preventDefault();
+    return this.props.onClick(key);
+  }
+
   render() {
+    const { amount, base, currency, currencyList, rates } = this.props;
+
     return (
       <div className="exchange-list">
-
-        <div className="exchange-item">
-          <div className="row">
-            <div className="col-xs-11">
-              <div className="row">
-                <div className="col-auto">
-                  <div className="exchange-item__currency">IDR</div>
-                </div>
-                <div className="col-auto end-xs">
-                  <div className="exchange-item__amount">144.104.50</div>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-xs-12">
-                  <p className="exchange-item__currency-desc">IDR - Indonesian Rupiah</p>
-                  <p className="exchange-item__currency-rate">1 USD = IDR 14.500</p>
-                </div>
-              </div>
-            </div>
-            <div className="d-flex col-xs-1 middle-xs">
-              <a>
-                <img src="/public/icons/remove.png" />
-              </a>
-            </div>
-          </div>
-        </div>
-
-        <div className="exchange-item">
-          <div className="row">
-            <div className="col-xs-11">
-              <div className="row">
-                <div className="col-auto">
-                  <div className="exchange-item__currency">IDR</div>
-                </div>
-                <div className="col-auto end-xs">
-                  <div className="exchange-item__amount">144.104.50</div>
+        {currencyList.map((row, key) =>
+          <Fragment key={key}>
+            {row !== base &&
+              <div className="exchange-item">
+                <div className="row">
+                  <div className="col-xs-11">
+                    <div className="row">
+                      <div className="col-auto">
+                        <div className="exchange-item__currency">{row}</div>
+                      </div>
+                      <div className="col-auto end-xs">
+                        <div className="exchange-item__amount">{amount * rates[row]}</div>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-xs-12">
+                        <p className="exchange-item__currency-desc">{row} - {find(currency, { name: row }).desc}</p>
+                        <p className="exchange-item__currency-rate">1 {base} = {row} {rates[row]}</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="d-flex col-xs-1 middle-xs">
+                    <a href="#" onClick={(e) => this.removeCurrency(e, key)}>
+                      <img src="/public/icons/remove.svg" />
+                    </a>
+                  </div>
                 </div>
               </div>
-              <div className="row">
-                <div className="col-xs-12">
-                  <p className="exchange-item__currency-desc">IDR - Indonesian Rupiah</p>
-                  <p className="exchange-item__currency-rate">1 USD = IDR 14.500</p>
-                </div>
-              </div>
-            </div>
-            <div className="d-flex col-xs-1 middle-xs">
-              <a>
-                <img src="/public/icons/remove.png" />
-              </a>
-            </div>
-          </div>
-        </div>
-
+            }
+          </Fragment>
+        )}
       </div>
     );
   }
 }
 
-export default ExchangeResult;
+ExchangeResult.propTypes = {
+  currencyList: PropTypes.array.isRequired,
+  rates: PropTypes.object.isRequired,
+  onClick: PropTypes.func.isRequired
+}
+
+const mapStateToProps = (state) => ({
+  currency: state.currency.data
+})
+
+export default connect(mapStateToProps)(ExchangeResult);
