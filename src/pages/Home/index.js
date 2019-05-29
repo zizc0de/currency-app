b1e224db8bd88724ce95a27f80cf7860246b1961
zizc0de <zizcode.exporadev@gmail.com>
2019-05-29 15:17:28 +0700

    Modified home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,72 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { exchangeActions } from 'actions';
 
 import ExchangeResult from 'components/ExchangeResult';
 import Navbar from 'components/Navbar';
 
+import { find } from 'lodash';
+
 class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      base: 'USD',
+      amount: 10,
+      currency: ['IDR', 'EUR', 'GBP', 'SGD'],
+      newCurrency: '',
+      form: false
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentWillMount() {
+    this.getRates(this.state.base);
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+
+    if (name === 'base') {
+      this.getRates(value);
+    }
+  }
+
+  removeCurrency = (key) => {
+    this.setState(state => ({
+      ...state,
+      currency: state.currency.filter((item, j) => key !== j)
+    }));
+  }
+
+  onSubmit = () => {
+    const { newCurrency } = this.state;
+
+    if (newCurrency) {
+      this.setState(state => ({
+        ...state,
+        currency: [...state.currency, newCurrency],
+        form: false
+      }));
+    }
+  }
+
+  getRates = (base) => {
+    this.props.getRates({
+      base
+    });
+  }
+
   render() {
+    const { currency, rates, asyncState } = this.props;
+    const { base, amount, form } = this.state;
+
     return (
       <div className="container">
         <Navbar />
@@ -14,22 +76,22 @@ class Home extends Component {
             <div className="col-xs-12">
               <div className="card">
                 <div className="card-title">
-                  <span>USD - United States Dollar</span>
+                  <span>{base} - {find(currency.data, { name: base }).desc}</span>
                 </div>
                 <div className="card-body">
                   <div className="row">
                     <div className="col-xs-6">
                       <div className="select-wrapper">
-                        <select className="form-control">
-                          <option value="USD">USD</option>
-                          <option value="IDR">IDR</option>
-                          <option value="SGD">SGD</option>
+                        <select name="base" className="form-control" value={base} onChange={this.handleChange}>
+                          {currency.data.map((row, key) =>
+                            <option value={row.name} key={key}>{row.name}</option>
+                          )}
                         </select>
                       </div>
                     </div>
                     <div className="col-xs-6">
                       <div className="w-100">
-                        <input type="text" className="form-control text-align-right" placeholder="Amount" />
+                        <input type="text" name="amount" value={amount} onChange={this.handleChange} className="form-control text-align-right" placeholder="Amount" />
                       </div>
                     </div>
                   </div>
@@ -50,51 +112,74 @@ class Home extends Component {
             <div className="col-xs-12">
               <div className="card py-0">
                 <div className="card-body">
-                  <ExchangeResult />
+                  {asyncState === 'idle' || asyncState === 'loading' ? (
+                    <div className="row">
+                      <div className="col-xs-12">
+                        <p>Loading..</p>
+                      </div>
+                    </div>
+                  ) : (
+                      <ExchangeResult
+                        amount={amount}
+                        base={base}
+                        currencyList={this.state.currency}
+                        rates={rates}
+                        onClick={(key) => this.removeCurrency(key)}
+                      />
+                    )}
                 </div>
               </div>
             </div>
           </div>
 
-          <section>
-            <div className="row mt-3">
-              <div className="col-xs-12">
-                <button type="button" className="btn btn-primary btn-block"><img src="/public/icons/add.png" /> Add More Currencies</button>
-              </div>
-            </div>
-          </section>
-
-          {/* <section>
-            <div className="row">
-              <div className="col-xs-12">
-                <p className="color-grey">Add More Currencies</p>
-              </div>
-            </div>
-          </section>
-
-          <div className="row">
-            <div className="col-xs-12">
-              <div className="card">
-                <div className="card-body">
-                  <div className="row">
-                    <div className="col-xs-12">
-                      <div className="select-wrapper">
-                        <select className="form-control">
-                          <option value="USD">IDR - Indonesian Rupiah</option>
-                          <option value="SGD">SGD</option>
-                        </select>
-                      </div>
-                    </div>
+          {form || this.state.currency.length === 0 ? (
+            <Fragment>
+              <section>
+                <div className="row">
+                  <div className="col-xs-12">
+                    <p className="color-grey">Add More Currencies</p>
                   </div>
-                  <div className="row mt-3">
-                    <div className="col-xs-12">
-                      <button type="button" className="btn btn-primary btn-block">Submit</button>
+                </div>
+              </section>
+              <div className="row">
+                <div className="col-xs-12">
+                  <div className="card">
+                    <div className="card-body">
+                      <div className="row">
+                        <div className="col-xs-12">
+                          <div className="select-wrapper">
+                            <select name="newCurrency" className="form-control" onChange={this.handleChange}>
+                              <option value="">Choose Currency</option>
+                              {currency.data.map((row, key) =>
+                                <Fragment key={`currency-${key}`}>
+                                  {this.state.currency.includes(row.name) === false &&
+                                    <option value={row.name}>{row.name} - {row.desc}</option>
+                                  }
+                                </Fragment>
+                              )}
+                            </select>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="row mt-3">
+                        <div className="col-xs-12">
+                          <button type="button" className="btn btn-primary btn-block" onClick={this.onSubmit}>Submit</button>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </div> */}
+            </Fragment>
+          ) : (
+              <section>
+                <div className="row mt-3">
+                  <div className="col-xs-12">
+                    <button type="button" className="btn btn-primary btn-block" onClick={() => this.setState({ form: true })}><img src="/public/icons/add.svg" /> Add More Currencies</button>
+                  </div>
+                </div>
+              </section>
+            )}
 
         </div>
       </div>
@@ -102,4 +187,14 @@ class Home extends Component {
   }
 }
 
-export default Home;
+const mapStateToProps = (state) => ({
+  asyncState: state.exchange.asyncState,
+  currency: state.currency,
+  rates: state.exchange.rates
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  getRates: bindActionCreators(exchangeActions.getRates, dispatch)
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
